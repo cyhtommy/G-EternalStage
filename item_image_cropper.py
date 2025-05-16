b6829d4eb2ac4a2d6cf51151e5e0f773f8df9c49
cyhtommy <cyhtommy@gmail.com>
2025-05-16 21:25:26 +0800

    Add files via upload

--- item_image_cropper.py
+++ item_image_cropper.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from PIL import Image
 import os
 
@@ -32,38 +31,3 @@ if __name__ == "__main__":
 
 
 
-=======
-from PIL import Image
-import os
-
-def batch_crop_images(input_folder, output_folder, crop_coordinates):
-    if not os.path.exists(output_folder):
-        os.makedirs(output_folder)
-    
-    for filename in os.listdir(input_folder):
-        if filename.endswith(('.jpg', '.jpeg', '.PNG')):
-            img_path = os.path.join(input_folder, filename)
-            with Image.open(img_path) as img:
-                # Crop the image
-                cropped_img = img.crop(crop_coordinates)
-                # Save the cropped image
-                cropped_img.save(os.path.join(output_folder, filename))
-                print(f"Cropped and saved: {filename}")
-
-if __name__ == "__main__":
-    # Define your crop coordinates (left, upper, right, lower)
-
-    os.chdir(os.path.dirname(os.path.abspath(__file__)))
-
-    crop_coords = (333, 457, 518, 642)  # Change this to your needs
-
-    # Define input and output folders
-    input_folder = 'image/'
-    output_folder = 'image/output'
-
-    batch_crop_images(input_folder, output_folder, crop_coords)
-
-
-
-
->>>>>>> c179494010e219a542fad61ecf25fd9f39d8791e
--- renamer.py
+++ renamer.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import os
 import pandas as pd
 import shutil
@@ -35,41 +34,3 @@ if os.path.exists(csv_name):
 
 
 
-=======
-import os
-import pandas as pd
-import shutil
-
-
-
-def copy_and_rename_file(source_path, destination_path):
-    # Copy the file to the destination
-    
-    
-    shutil.copy(source_path, destination_path)
-    print(f"Copied and renamed to: {destination_path}")
-
-os.chdir(os.path.dirname(os.path.abspath(__file__)))
-
-csv_name='unit_log.csv'
-
-if os.path.exists(csv_name):
-    df = pd.read_csv(csv_name)
-    df_copy = df.copy()
-    for index, row in df.iterrows():
-        img_id=row['ID']
-        title=row['Title']
-        filepath=row['File Path']
-        directory = os.path.dirname(filepath)
-        destination_path = os.path.join(directory, img_id)+'.webp'
-        copy_and_rename_file(filepath,destination_path)
-        
-        df_copy.loc[index, 'File Path'] =destination_path
-    df_copy.to_csv('output_file.csv', index=False)
-
-
-
-
-
-
->>>>>>> c179494010e219a542fad61ecf25fd9f39d8791e
